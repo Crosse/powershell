0f9f900e3f28a5e9fa1fa8d8aca1c0f3037fbdbe
Seth Wright <seth@crosse.org>
2010-04-19 23:15:17 +0000

    attempt to inline the output of Clean-TrainingMailboxes

--- exchange/Clean-TrainingMailboxes.ps1
+++ exchange/Clean-TrainingMailboxes.ps1
@@ -25,8 +25,6 @@
 ################################################################################
 
 $now = Get-Date
-$logFile = "training_$($now.Year)-$($now.Month)-$($now.Day).log"
-Start-Transcript $logFile
 
 # Get all of the training user mailboxes
 $mboxes = Get-Mailbox -Filter { Name -like "Training User*" } -RecipientTypeDetails UserMailbox
@@ -36,7 +34,3 @@ $mboxes | Disable-Mailbox -Confirm:$false
 Start-Sleep 120
 # Recreate the mailboxes
 $mboxes | Enable-Mailbox -Database it-exmbx1\Training -ManagedFolderMailboxPolicy "Default Managed Folder Policy" -ManagedFolderMailboxPolicyAllowed:$true
-Stop-Transcript
-
-# Send the log as an email attachment
-E:\Scripts\Send-Email.ps1 -From '"Exchange Maintenance Account" <it-exmaint@jmu.edu>' -To 'millerca@jmu.edu,moellejf@jmu.edu' -Cc 'wrightst@jmu.edu,najdziav@jmu.edu' -Subject 'Output of Training Mailbox Creation' -Body 'See attachment' -SmtpServer et.jmu.edu -SmtpPort 25 -AttachmentFile $logFile
