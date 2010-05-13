d8a70c123261ae5c2b9f0d3b50f077c0c3dae86d
Seth Wright <seth@crosse.org>
2010-05-13 21:22:35 +0000

    now pulls mailbox database size, total user count per database, outstanding transaction logs per database

--- exchange/Get-ExchangeInfo.ps1
+++ exchange/Get-ExchangeInfo.ps1
@@ -5,7 +5,6 @@
 # DESCRIPTION:  Sends an email with relevant Exchange statistics to various
 #               users.
 #
-# 
 # Copyright (c) 2009,2010 Seth Wright <wrightst@jmu.edu>
 #
 # Permission to use, copy, modify, and distribute this software for any
@@ -22,59 +21,109 @@
 #
 ################################################################################
 
-#start-transcript exchangeinfo.log
+# Change these to suit your environment
+$SmtpServer = "it-exhub.ad.jmu.edu"
+$From       = "it-exmaint@jmu.edu"
+$To         = "wrightst@jmu.edu, gumgs@jmu.edu, liskeygn@jmu.edu, stockntl@jmu.edu, flynngn@jmu.edu, najdziav@jmu.edu"
+#$To         = "wrightst@jmu.edu"
+$Title      = "Exchange User Detail for $(Get-Date -Format d)"
 
-$From = "Exchange System <it-exmaint@jmu.edu>"
-$To = "wrightst@jmu.edu, gumgs@jmu.edu, stockntl@jmu.edu, flynngn@jmu.edu, kingms@jmu.edu, mastrw@jmu.edu, najdziav@jmu.edu"
-$Title = "Exchange User Detail for $(Get-Date -Format d)"
-
-$everything   = Get-Mailbox -Server it-exmbx1 -ResultSize Unlimited
-$allMailboxes = $everything | where { $_.OrganizationalUnit -notmatch 'ITTraining' -and $_.OrganizationalUnit -notmatch 'HelpDesk' }
-
-$ittraining   = ($everything | where { $_.OrganizationalUnit -match 'ITTraining' }).Count
-$helpdesk     = ($everything | where { $_.OrganizationalUnit -match 'HelpDesk' }).Count
-
-$resources    = ($allMailboxes | Where { $_.RecipientTypeDetails -match "RoomMailbox" -or $_.RecipientTypeDetails -match "EquipmentMailbox" }).Count
-$shared       = ($allMailboxes | Where { $_.RecipientTypeDetails -match "SharedMailbox" }).Count
-
-$users        = $allMailboxes | where { $_.RecipientTypeDetails -match 'UserMailbox' -and $_.OrganizationalUnit -match "JMUma" } | Sort
-
-$ou           = [ADSI]"LDAP://OU=PhishingReplyAddresses,OU=ExchangeObjects,DC=ad,DC=jmu,DC=edu"
-$antiphishing = 0
-foreach ($entry in $ou.psbase.Children) { $antiphishing++ }
-Remove-Variable ou
+##################################
+$cwd = [System.IO.Path]::GetDirectoryName(($MyInvocation.MyCommand).Definition)
 
 $dls = (Get-DistributionGroup).Count
+$resources = 0
+$shared = 0
+$users = 0
+$totalStorage = 0
 
-$dbs = New-Object System.Text.StringBuilder
-$everything | Group-Object Database | Sort Count,Name -Descending | % { $null = $dbs.AppendFormat("{0,9}`t{1}`n", $_.Count, $_.Name) }
+$dbInfoArray = New-Object System.Collections.ArrayList
+Get-MailboxServer | % { 
+    $CMSName = $_.Name
+    $OperationalMachines = (Get-ClusteredMailboxServerStatus -Identity $CMSName).OperationalMachines
+    $null = $OperationalMachines | where { $_ -match "^(?<activenode>.*)\s+<Active.*" }
+    
+    if (!($matches.activenode)) {
+        # No regex matches were found.
+        $message = "Cannot determine the Active Node of CMS $CMSName"
+        continue
+    }
+
+    $ActiveNode = $matches.activenode
+    $preamble = "\\" + $ActiveNode + "\"
+
+    $sgs = Get-StorageGroup -Server $CMSName
+    $i = 1
+    foreach ($sg in $sgs) {
+        $percent = $([int]($i/$sgs.Count*100))
+        Write-Progress -Activity "Processing Storage Groups on $CMSName" `
+            -Status "$percent% Complete" `
+            -PercentComplete $percent -CurrentOperation "Processing $($sg.Identity)"
+        $i++
+        
+        foreach ($db in (Get-MailboxDatabase -StorageGroup $sg)) { 
+            $dbInfo = New-Object PSObject
+            $dbInfo = Add-Member -PassThru -InputObject $dbInfo NoteProperty Identity $null
+            Add-Member -InputObject $dbInfo NoteProperty UserCount -1
+            Add-Member -InputObject $dbInfo NoteProperty LogCount -1
+            Add-Member -InputObject $dbInfo NoteProperty DbSize -1
+
+            $dbInfo.Identity = $db.Identity
+
+            $logPath = $preamble + $sg.LogFolderPath.ToString().Replace(":", "$")
+            $dbInfo.LogCount = (gci "$logPath\*.log").Count
+
+            $edbFilePath = $preamble + $db.EdbFilePath.ToString().Replace(":", "$")
+            $dbSize = [Math]::Floor( (gci $edbFilePath).Length / 1MB )
+            $totalStorage += $dbSize
+            $dbInfo.DbSize = $dbSize
+
+            $mboxes = Get-Mailbox -ResultSize Unlimited -Database $db
+            $dbInfo.UserCount = $mboxes.Count
+
+            $mboxes | % { 
+                switch ($_.RecipientTypeDetails) {
+                    'UserMailbox' { 
+                        $users++
+                        break
+                    }
+                    'SharedMailbox' { 
+                        $shared++
+                        break
+                    }
+                    'EquipmentMailbox' { 
+                        $resources++
+                        break
+                    }
+                    'RoomMailbox' {
+                        $resources++
+                        break
+                    }
+                    default { }
+                }
+            }
+                        
+            $null = $dbInfoArray.Add($dbInfo)
+        }
+    }
+}
 
 $Body  = @"
-Total Mailbox Count:	$($everything.Count)
-     User Mailboxes:	$($users.Count)
+     User Mailboxes:	$users
    Shared Mailboxes:	$shared
-          Resources:	$resources
+ Resource Mailboxes:	$resources
 Distribution Groups:	$dls
-
- Training Mailboxes:	$ittraining
- HelpDesk Mailboxes:	$helpdesk
-
-Known Phishing Addresses Blacklisted:  $antiphishing
+ Total Storage Used:    $totalStorage MB
 
 
-Database Utilization:
-
-Mailboxes`tDatabase
----------`t--------
-$($dbs.ToString())
+$($dbInfoArray | Sort Identity | ft -AutoSize | Out-String)
 "@
 
-$SmtpServer = "it-exhub.ad.jmu.edu"
-$SmtpClient = New-Object System.Net.Mail.SmtpClient
-$SmtpClient.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
-$SmtpClient.Port = 25
-$SmtpClient.host = $SmtpServer
-$SmtpClient.Send($From, $To, $Title, $Body)
+$attachedFile = gci ([System.IO.Path]::GetTempFileName())
+$attachedFile.MoveTo("$([System.IO.Path]::GetFileNameWithoutExtension($attachedFile.Name)).csv")
 
-#stop-transcript
+$dbInfoArray | Sort Identity | Export-Csv -NoTypeInformation -Encoding ASCII -Path $attachedFile
 
+& "$cwd\Send-Email.ps1" -From $From -To $To -Subject $Title -Body $Body -AttachmentFile $attachedFile -SmtpServer $SmtpServer 
+
+Remove-Item $attachedFile
