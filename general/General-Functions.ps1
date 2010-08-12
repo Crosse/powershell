3d56a6dfcd8a3c1f239950cef41dcd976f79fbfb
Seth Wright <seth@crosse.org>
2010-08-12 14:20:19 +0000

    removed base64-functions.ps1 and moved content into general-functions.ps1

--- general/General-Functions.ps1
+++ general/General-Functions.ps1
@@ -49,3 +49,29 @@ function global:Get-HexDump($Path,$Width=10, $Bytes=-1)
 }
 
 Write-Host "`tAdded Get-HexDump to global functions." -Fore White
+
+
+if (Test-Path function:ConvertTo-Base64) { 
+    Remove-Item function:ConvertTo-Base64
+}
+function global:ConvertTo-Base64($string) {
+   $bytes  = [System.Text.Encoding]::UTF8.GetBytes($string);
+   $encoded = [System.Convert]::ToBase64String($bytes); 
+
+   return $encoded;
+}
+
+Write-Host "`tAdded ConvertTo-Base64 to global functions." -Fore White
+
+
+if (Test-Path function:ConvertFrom-Base64) { 
+    Remove-Item function:ConvertFrom-Base64
+}
+function global:ConvertFrom-Base64($string) {
+   $bytes  = [System.Convert]::FromBase64String($string);
+   $decoded = [System.Text.Encoding]::UTF8.GetString($bytes); 
+
+   return $decoded;
+}
+
+Write-Host "`tAdded ConvertFrom-Base64 to global functions." -Fore White
