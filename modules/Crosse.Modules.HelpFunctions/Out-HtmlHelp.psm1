c25c777ecd2966cb5f651ad8f19b237cf7768bcd
Seth Wright <seth@crosse.org>
2010-08-18 02:31:52 +0000

    modified Out-HtmlHelp to use the HtmlDocument class to make the script prettier.

--- modules/Crosse.Modules.HelpFunctions/Out-HtmlHelp.psm1
+++ modules/Crosse.Modules.HelpFunctions/Out-HtmlHelp.psm1
@@ -30,203 +30,201 @@ function Out-HtmlHelp {
 
             [string]
             # The output directory.
-            $OutputDir="$(Get-Location)\Help"
+            $OutputDir="$(Get-Location)\Help",
+
+            [switch]
+            # Whether to copy the template CSS file to the output directory.
+            $CopyCssTemplate=$true
           )
 
     BEGIN {
         if ((Test-Path $OutputDir) -eq $false) {
+            Write-Host "Making output directory"
             mkdir $OutputDir | Out-Null
+            Write-Host "PSScriptRoot = $PSScriptRoot"
         }
-        Push-Location
-        cd $OutputDir
+
+        if ($CopyCssTemplate) {
+            Write-Host "Copying template CSS file to output directory"
+            copy "$PSScriptRoot\powershell-help.css" $OutputDir
+        }
     }
 
     PROCESS {
-        $fileName = $HelpInfo.Name + ".html"
-        $fileText = @"
-        <html>
-        <head>
-            <title>$($HelpInfo.Name)</title>
-            <link rel="stylesheet" type="text/css" href="powershell-help.css" />
-        </head>
+        $fileName = Join-Path $OutputDir $($HelpInfo.Name + ".html")
+        Write-Host "Writing help for `"$($HelpInfo.Name)`" to file $fileName"
 
-        <body>
-            <h1>$(TidyString $HelpInfo.Name)</h1>
-            <h2>SYNOPSIS</h2>
-                <div class="cmdSynopsis">$(TidyString $HelpInfo.Synopsis)</div>
-            <h2>SYNTAX</h2>
-"@
+        $doc = New-Object Crosse.Net.HtmlDocument
+        $doc.Title = "$($HelpInfo.Name)"
+        $doc.StyleSheet = "powershell-help.css"
+        
+        $doc.WriteHeading(1, $HelpInfo.Name)
 
+        $doc.WriteHeading(2, "SYNOPSIS")
+        $doc.WriteDiv("cmdSynopsis", $HelpInfo.Synopsis)
+        $doc.WriteLine()
+
+        $doc.WriteHeading(2, "SYNTAX")
         foreach ($item in $HelpInfo.syntax.syntaxItem) {
-            $params = @"
-                <div class="cmdSyntax">
-                    $(TidyString $item.name) 
-"@
+            $doc.BeginDiv("cmdSyntax")
+            $doc.Write($item.name + " ")
+
             foreach ($p in $item.parameter) {
                 if ($p.required -ne "true") {
-                    $params += "["
+                    $doc.Write("[")
                 }
 
-                $params += "-$(TidyString $p.name)"
+                $doc.Write("-$($p.name)")
                 if ($p.parameterValue -ne $null) {
-                    $params+= " &lt;$(TidyString $p.parameterValue)&gt;"
+                    $doc.Write(" <$($p.parameterValue)>")
                 }
 
                 if ($p.required -ne "true") {
-                    $params += "]"
+                    $doc.Write("]")
                 }
 
-                $params += " "
+                $doc.Write(" ")
             }
-            $fileText += $params
-            $fileText += @"
-                </div>
-                <br />
-"@
+            $doc.EndDiv()
+            $doc.WriteLine()
+            $doc.WriteBreak()
+            $doc.WriteLine()
         }
 
-        $fileText +=@"
-            <h2>DESCRIPTION</h2>
-            <div class="cmdDescription">
-"@
-
+        $doc.WriteHeading(2, "DESCRIPTION")
+        $doc.BeginDiv("cmdDescription")
         foreach ($d in $HelpInfo.description) {
-            $desc = "                "
-            $desc += $d.Tag + $d.Text
-            $desc += "`n                <p />"
-            $fileText += $desc
+            $doc.WriteLine()
+            $doc.WriteLine($d.Tag + $d.Text)
+            $doc.NewParagraph()
+            $doc.WriteLine()
         }
+        $doc.EndDiv()
+        $doc.WriteLine()
 
-        $fileText += @"
-            </div>
-            <h2>PARAMETERS</h2>
-"@
-
-
+        $doc.WriteHeading(2, "PARAMETERS")
         foreach ($p in $HelpInfo.parameters.parameter) {
-            $fileText += @"
-            <div class="cmdParameter">
-                <div class="cmdParameterName">-$(TidyString $p.name) [&lt;$(TidyString $p.parameterValue)&gt;]</div>
-                <div class="cmdParameterDesc">
-"@
+            $doc.BeginDiv("cmdParameter")
+            $doc.WriteLine()
+            $doc.BeginDiv("cmdParameterName")
+            $doc.Write("-$($p.name) ")
 
-            foreach ($d in $p.description) {
-                $desc = "                "
-                $desc += $d.Tag + $d.Text
-                $desc += "`n                <br />"
-                $fileText += $desc
+            if ($p.parameterValue -eq "SwitchParameter") {
+                $doc.Write("[")
             }
 
-            $fileText += @"
-                </div>
-                <table class="cmdParameterAttr">
-                    <tr>
-                        <td class="cmdParameterAttrName">Required?</td>
-                        <td class="cmdParameterAttrValue">$($p.required)</td>
-                    </tr>
-                    <tr>
-                        <td class="cmdParameterAttrName">Position?</td>
-                        <td class="cmdParameterAttrValue">$($p.position)</td>
-                    </tr>
-                    <tr>
-                        <td class="cmdParameterAttrName">Accept pipeline input?</td>
-                        <td class="cmdParameterAttrValue">$($p.pipelineInput)</td>
-                    </tr>
-                </table>
-            </div>
-"@
+            $doc.Write("<$($p.parameterValue)>")
+
+            if ($p.parameterValue -eq "SwitchParameter") {
+                $doc.Write("]")
+            }
+            $doc.EndDiv()
+            $doc.WriteLine()
+
+            $doc.BeginDiv("cmdParameterDesc")
+            foreach ($d in $p.description) {
+                $doc.WriteLine($d.Tag + $d.Text)
+                $doc.WriteBreak()
+            }
+            $doc.WriteLine()
+            $doc.EndDiv()
+            $doc.WriteLine()
+
+            $doc.BeginTable("cmdParameterAttr")
+            
+            $doc.BeginTableRow()
+            $doc.BeginTableColumn("cmdParameterAttrName")
+            $doc.Write("Required?")
+            $doc.EndTableColumn()
+            $doc.BeginTableColumn("cmdParameterAttrValue")
+            $doc.Write($p.required)
+            $doc.EndTableColumn()
+            $doc.EndTableRow()
+
+            $doc.BeginTableRow()
+            $doc.BeginTableColumn("cmdParameterAttrName")
+            $doc.Write("Position?")
+            $doc.EndTableColumn()
+            $doc.BeginTableColumn("cmdParameterAttrValue")
+            $doc.Write($p.position)
+            $doc.EndTableColumn()
+            $doc.EndTableRow()
+
+            $doc.BeginTableRow()
+            $doc.BeginTableColumn("cmdParameterAttrName")
+            $doc.Write("Accept pipeline input?")
+            $doc.EndTableColumn()
+            $doc.BeginTableColumn("cmdParameterAttrValue")
+            $doc.Write($p.pipelineInput)
+            $doc.EndTableColumn()
+            $doc.EndTableRow()
+
+            $doc.EndTable()
+            $doc.EndDiv()
+            $doc.WriteLine()            
         }
 
 
-        $fileText += @"
-            <h2>INPUTS</h2>
-                <div class="cmdInputs">$(TidyString $HelpInfo.inputTypes.inputType.type.name)</div>
-            <h2>OUTPUTS</h2>
-                <div class="cmdOutputs">$(TidyString $HelpInfo.returnValues.returnValue.type.name)</div>
-            <h2>EXAMPLES</h2>
-"@
+        $doc.WriteHeading(2, "INPUTS")
+        $doc.BeginDiv("cmdInputs")
+        $doc.WriteLine($HelpInfo.inputTypes.inputType.type.name)
+        $doc.EndDiv()
 
+        $doc.WriteHeading(2, "OUTPUTS")
+        $doc.BeginDiv("cmdOutputs")
+        $doc.WriteLine($HelpInfo.returnValues.returnValue.type.name)
+        $doc.EndDiv()
+
+        $doc.WriteHeading(2, "EXAMPLES")
         foreach ($e in $HelpInfo.examples.example) {
-            $example = @"
-                <div class="cmdExample">
-                    $(TidyString $e.title)<br />
+            $doc.BeginDiv("cmdExample")
+            $doc.WriteLine()
+            $doc.WriteLine($e.title)
+            $doc.WriteBreak()
+            $doc.WriteLine()
 
-"@
             foreach ($c in $e.code) {
-                $example += @"
-                    $(TidyString $c)<br />
-
-"@
+                $doc.WriteLine($c)
+                $doc.WriteBreak()
+                $doc.WriteLine()
             }
             
             foreach ($r in $e.remarks) {
-                $example += @"
-                    $(TidyString $r.Text)<br />
-
-"@
+                if (![String]::IsNullOrEmpty($r.Text)) {
+                    foreach ($l in $r.Text.Split("`n")) {
+                        $doc.WriteLine($l)
+                        $doc.WriteBreak()
+                        $doc.WriteLine()
+                    }
+                }
+                $doc.WriteLine()
             }
             
-            $example += @"
-                </div>
-"@
-            $fileText += $example
+            $doc.EndDiv()
+            $doc.WriteLine()
         }
 
-        $fileText += @"
-            <h2>RELATED LINKS</h2>
-            <div class="cmdRelatedLinks">
-"@
-
+        $doc.WriteHeading(2, "RELATED LINKS")
+        $doc.BeginDiv("cmdRelatedLinks")
         foreach ($link in $HelpInfo.relatedLinks.navigationLink) {
             if ([String]::IsNullOrEmpty($link.linkText)) { 
-                $linkText = TidyString $link.uri
+                $linkText = $link.uri
             } else { 
-                $linkText = TidyString $link.linkText
+                $linkText = $link.linkText
             }
 
-            $fileText += @"
-                <a href="$($link.uri)" class="cmdRelatedLink">$linkText</a>
-                <p />
-"@
+            $doc.WriteLink("cmdRelatedLink", $link.uri, $linkText)
+            $doc.NewParagraph()
         }
+        $doc.EndDiv()
 
-        $fileText += @"
-           </div>
-           <br />
-           <br />
-        </body>
-    </html>
-"@
-
-        $fileText | Out-File $fileName -Encoding ASCII
+        $doc.WriteBreak();
+        $doc.WriteBreak();
+        
+        $doc.ToString() | Out-File $fileName -Encoding ASCII
     }
 
     END {
-        Pop-Location
+        Write-Host "Finished."
     }
 }
-
-function TidyString {
-    param (
-            [Parameter(ValueFromPipeline=$true)]
-            # The string to convert.
-            $Object
-          )
-
-        if ($Object -eq $null) {
-            return $null
-        }
-
-        $retval = $Object.ToString()
-        $retval = $retval.Trim()
-        while ($retval[-1] -eq "`n") {
-            $retval.Remove($retval.Length -1, 1)
-        }
-        $retval = $retval.Replace("&", "&amp;")
-        $retval = $retval.Replace("<", "&lt;")
-        $retval = $retval.Replace(">", "&gt;")
-        $retval = $retval.Replace("`n", "<br />`n")
-
-        return $retval
-}
-
