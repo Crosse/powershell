ee04264480db7362a2677913ab08bda714939a18
Seth Wright <seth@crosse.org>
2011-10-15 18:50:13 -0400

    Some cleanup in Get-ComputerStatus to make the results look better.

--- scripts/management/Get-ComputerStatus.ps1
+++ scripts/management/Get-ComputerStatus.ps1
@@ -17,39 +17,44 @@ PROCESS {
     Add-Member -InputObject $result -MemberType NoteProperty -Name "PingStatus" -Value $null
     Add-Member -InputObject $result -MemberType NoteProperty -Name "PingRoundTripTime" -Value $null
     Add-Member -InputObject $result -MemberType NoteProperty -Name "WmiStatus" -Value $null
+    Add-Member -InputObject $result -MemberType NoteProperty -Name "WmiError" -Value $null
     Add-Member -InputObject $result -MemberType NoteProperty -Name "Manufacturer" -Value $null
     Add-Member -InputObject $result -MemberType NoteProperty -Name "Model" -Value $null
     Add-Member -InputObject $result -MemberType NoteProperty -Name "OSVersion" -Value $null
     Add-Member -InputObject $result -MemberType NoteProperty -Name "OSName" -Value $null
     Add-Member -InputObject $result -MemberType NoteProperty -Name "PSRemotingStatus" -Value $null
     Add-Member -InputObject $result -MemberType NoteProperty -Name "PSRemotingError" -Value $null
+
+    Write-Verbose "$ComputerName : starting."
     
     try {
         $dns = [System.Net.Dns]::GetHostEntry($ComputerName)
         $result.DnsStatus = "Found"
         $result.HostName = $dns.HostName
     } catch {
+        $result.DnsStatus = "NotFound"
         Write-Verbose "$ComputerName : Name not found in DNS."
         return $result
     }
 
     $pingResult = $ping.Send($ComputerName, 1000)
+    $result.PingStatus = $pingResult.Status
+    $result.PingRoundTripTime = $pingResult.RoundTripTime
+
     if ($pingResult.Status -ne 'Success') {
         Write-Verbose "$ComputerName : Did not respond to ping."
         return $result
-    } else {
-        $result.PingStatus = $pingResult.Status
-        $result.PingRoundTripTime = $pingResult.RoundTripTime
     }
 
     $error.Clear()
     $computerSystem = Get-WmiObject -ComputerName $ComputerName -Class Win32_ComputerSystem
     if ($computerSystem -eq $null) {
         Write-Verbose "$ComputerName : Could not connect to WMI (Win32_ComputerSystem)"
-        $result.WmiStatus = $error[0].Exception
+        $result.WmiStatus = "Failed (ComputerSystem)"
+        $result.WmiError = $error[0].Exception
         return $result
     } else {
-        $result.WmiStatus = "Success"
+        $result.WmiStatus = "Success (ComputerSystem)"
         $result.Manufacturer = $computerSystem.Manufacturer
         $result.Model = $computerSystem.Model
     }
@@ -58,10 +63,11 @@ PROCESS {
     $operatingSystem = Get-WmiObject -ComputerName $ComputerName -Class Win32_OperatingSystem
     if ($operatingSystem -eq $null) {
         Write-Verbose "$ComputerName : Could not connect to WMI (Win32_OperatingSystem)"
-        $result.WmiStatus = $error[0].Exception
+        $result.WmiStatus += "/Failed (OperatingSystem)"
+        $result.WmiError += $error[0].Exception
         return $result
     } else {
-        $result.WmiStatus = "Success"
+        $result.WmiStatus += "/Success (OperatingSystem)"
         $result.OSVersion = $operatingSystem.Version
         $result.OSName = $operatingSystem.Name.Split("|")[0]
     }
