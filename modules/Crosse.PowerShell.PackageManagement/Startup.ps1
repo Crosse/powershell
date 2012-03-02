7286138477cb80e78ae7efaed4b222fcec3ee007
Seth Wright <seth@crosse.org>
2012-03-02 14:44:08 -0500

    Expose a few more Package properties straight off the PackageFile object

--- modules/Crosse.PowerShell.PackageManagement/Startup.ps1
+++ modules/Crosse.PowerShell.PackageManagement/Startup.ps1
@@ -33,13 +33,37 @@ using System.IO;
 using System.IO.Packaging;
 
 namespace Crosse.PowerShell.PackageManagement {
-    public class PackageFile {
+    public class PackageFile : IDisposable {
         public string FileName { get; internal set; }
+        public string Creator { get { return this.Package.PackageProperties.Creator; } }
+        public string Title { get { return this.Package.PackageProperties.Title; } }
+        public string Subject { get { return this.Package.PackageProperties.Subject; } }
+        public string Category { get { return this.Package.PackageProperties.Category; } }
+        public string Keywords { get { return this.Package.PackageProperties.Keywords; } }
+        public string Description { get { return this.Package.PackageProperties.Description; } }
+        public string Version { get { return this.Package.PackageProperties.Version; } }
+        public string Revision { get { return this.Package.PackageProperties.Revision; } }
+        public DateTime? Created { get { return this.Package.PackageProperties.Created; } }
+        public DateTime? Modified { get { return this.Package.PackageProperties.Modified; } }
+        public string LastModifiedBy { get { return this.Package.PackageProperties.LastModifiedBy; } }
+        public string Identifier { get { return this.Package.PackageProperties.Identifier; } }
         public System.IO.Packaging.Package Package { get; internal set; }
 
         public PackageFile(string fileName, FileMode mode) {
             this.Package = System.IO.Packaging.Package.Open(fileName, mode);
             this.FileName = fileName;
+        }
+
+        public void Close() {
+            if (this.Package != null) {
+                this.Package.Close();
+            }
+            this.Package = null;
+            this.FileName = null;
+        }
+
+        public void Dispose() {
+            Close();
         }
     }
 }
