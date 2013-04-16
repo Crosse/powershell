4f56b7f59b058706b3f51c257efbc09d5b36d546
Seth Wright <seth@crosse.org>
2013-04-15 21:09:00 -0400

    Add the ISC license and cmdlet help.

--- modules/Crosse.PowerShell.Media/Out-M4V.psm1
+++ modules/Crosse.PowerShell.Media/Out-M4V.psm1
@@ -1,3 +1,37 @@
+################################################################################
+#
+# Copyright (c) 2013 Seth Wright <seth@crosse.org>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+################################################################################
+
+################################################################################
+<#
+    .SYNOPSIS
+    Transcodes a source video file.
+
+    .DESCRIPTION
+    Transcodes a source video file into a destination MP4 or MKV file.
+
+    .INPUTS
+    System.String. The names of multiple source files to transcode can be passed via the command line.
+
+    .OUTPUTS
+    None.
+
+#>
+################################################################################
 function Out-M4V {
     [CmdletBinding()]
     param (
