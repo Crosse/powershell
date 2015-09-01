b0860938d1171798ff02bb7db1d2a4c177875a65
Seth Wright <seth@crosse.org>
2015-09-01 11:17:26 -0400

    This file has changed too much over the past few years.
    I can't tell what is what between whitespace and logic changes,
    but this is the file that is used in production.

--- scripts/exchange/Set-MailboxQuotaLimits.ps1
+++ scripts/exchange/Set-MailboxQuotaLimits.ps1
@@ -28,7 +28,10 @@ param (
 
         [Parameter(Mandatory=$false)]
         [IO.FileInfo]
-        $TranscriptPath
+        $TranscriptPath,
+
+        [switch]
+        $Force
       )
 
 # This section executes only once, before the pipeline.
@@ -69,70 +72,70 @@ PROCESS {
         return
     }
 
-    if ($stats.StorageLimitStatus -ne 'NoChecking' -and 
-            $stats.StorageLimitStatus -ne 'BelowLimit') {
-        Write-Verbose "Verifying mailbox quota limits for `"$($Mailbox.Name)`" (current size: $($stats.TotalItemSize.Value.ToMB()) MB)"
-        if ($Mailbox.UseDatabaseQuotaDefaults -eq $true) {
-            $db = Get-MailboxDatabase $Mailbox.Database `
-                        -DomainController $dc
-
-            Write-Verbose "Mailbox is using database quota defaults:"
-            $IssueWarning           = $db.IssueWarningQuota.Value.ToMB()
-            $ProhibitSend           = $db.ProhibitSendQuota.Value.ToMB()
-            $ProhibitSendReceive    = $db.ProhibitSendReceiveQuota.Value.ToMB()
-        } else {
-            Write-Verbose "Mailbox has custom quota levels:"
-            $IssueWarning           = $Mailbox.IssueWarningQuota.Value.ToMB()
-            $ProhibitSend           = $Mailbox.ProhibitSendQuota.Value.ToMB()
-            $ProhibitSendReceive    = $Mailbox.ProhibitSendReceiveQuota.Value.ToMB()
-        }
-
-        Write-Verbose "Current Quota Limits for mailbox:"
-        Write-Verbose "IssueWarningQuota:         $($IssueWarning)MB"
-        Write-Verbose "ProhibitSendQuota:         $($ProhibitSend)MB"
-        Write-Verbose "ProhibitSendReceiveQuota:  $($ProhibitSendReceive)MB"
-
-        $totalItemSize = [Math]::Floor($stats.TotalItemSize.Value.ToBytes()/1MB)
-        Write-Verbose "TotalItemSize:             $($totalItemSize)MB"
-
-        # Round up to the nearest 100MB.
-        $totalRoundedSize = ($totalItemSize - (($totalItemSize % 100) - 100))*1MB
-        Write-Verbose "Total Rounded Size:        $($totalRoundedSize/1MB)MB"
-
-        $IssueWarning           = $totalRoundedSize + $Headroom
-        $ProhibitSend           = $totalRoundedSize + $Headroom * 2
-        $ProhibitSendReceive    = $totalRoundedSize + $Headroom * 3
-
-        if ($IssueWarning -gt $UpperLimit) {
-            Write-Error "Raising the user's quota would exceed the requested upper limit."
-            return
-        }
-
-        Write-Verbose "Proposed Quota Limits for mailbox:"
-        Write-Verbose "IssueWarningQuota:         $($IssueWarning/1MB)MB"
-        Write-Verbose "ProhibitSendQuota:         $($ProhibitSend/1MB)MB"
-        Write-Verbose "ProhibitSendReceiveQuota:  $($ProhibitSendReceive/1MB)MB"
-
-        $desc = "Raise Quota for `"$Mailbox`""
-        $caption = $desc
-        $warning = "Are you sure you want to perform this action?`n"
-        $warning += "This will raise quota limits for the user "
-        $warning += "to $($IssueWarning/1MB)MB/$($ProhibitSend/1MB)MB/$($ProhibitSendReceive/1MB)MB.`n"
-        $warning += "Current mailbox size:  $($TotalItemSize)MB"
-
-        if (!$PSCmdlet.ShouldProcess($desc, $warning, $caption)) {
-            return
-        }
-
-        Set-Mailbox -Identity $Mailbox -DomainController $dc `
-                    -IssueWarningQuota $IssueWarning `
-                    -ProhibitSendQuota $ProhibitSend `
-                    -ProhibitSendReceiveQuota $ProhibitSendReceive `
-                    -UseDatabaseQuotaDefaults:$false
-        Write-Verbose "Raised quota limits for `"$($Mailbox.Name)`"."
-    } else {
+    if (($stats.StorageLimitStatus -eq 'NoChecking' -or $stats.StorageLimitStatus -eq 'BelowLimit') -and !$Force) {
         Write-Verbose "Mailbox for `"$($Mailbox.Name)`" is within quota limits."
+        return
     }
+
+    Write-Verbose "Verifying mailbox quota limits for `"$($Mailbox.Name)`" (current size: $($stats.TotalItemSize.Value.ToMB()) MB)"
+    if ($Mailbox.UseDatabaseQuotaDefaults -eq $true) {
+        $db = Get-MailboxDatabase $Mailbox.Database `
+                    -DomainController $dc
+
+        Write-Verbose "Mailbox is using database quota defaults:"
+        $IssueWarning           = $db.IssueWarningQuota.Value.ToMB()
+        $ProhibitSend           = $db.ProhibitSendQuota.Value.ToMB()
+        $ProhibitSendReceive    = $db.ProhibitSendReceiveQuota.Value.ToMB()
+    } else {
+        Write-Verbose "Mailbox has custom quota levels:"
+        $IssueWarning           = $Mailbox.IssueWarningQuota.Value.ToMB()
+        $ProhibitSend           = $Mailbox.ProhibitSendQuota.Value.ToMB()
+        $ProhibitSendReceive    = $Mailbox.ProhibitSendReceiveQuota.Value.ToMB()
+    }
+
+    Write-Verbose "Current Quota Limits for mailbox:"
+    Write-Verbose "IssueWarningQuota:         $($IssueWarning)MB"
+    Write-Verbose "ProhibitSendQuota:         $($ProhibitSend)MB"
+    Write-Verbose "ProhibitSendReceiveQuota:  $($ProhibitSendReceive)MB"
+
+    $totalItemSize = [Math]::Floor($stats.TotalItemSize.Value.ToBytes()/1MB)
+    Write-Verbose "TotalItemSize:             $($totalItemSize)MB"
+
+    # Round up to the nearest 100MB.
+    $totalRoundedSize = ($totalItemSize - (($totalItemSize % 100) - 100))*1MB
+    Write-Verbose "Total Rounded Size:        $($totalRoundedSize/1MB)MB"
+
+    $IssueWarning           = $totalRoundedSize + $Headroom
+    $ProhibitSend           = $totalRoundedSize + $Headroom * 2
+    $ProhibitSendReceive    = $totalRoundedSize + $Headroom * 3
+
+    if ($IssueWarning -gt $UpperLimit) {
+        Write-Error "Raising the user's quota would exceed the requested upper limit."
+        return
+    }
+
+    Write-Verbose "Proposed Quota Limits for mailbox:"
+    Write-Verbose "IssueWarningQuota:         $($IssueWarning/1MB)MB"
+    Write-Verbose "ProhibitSendQuota:         $($ProhibitSend/1MB)MB"
+    Write-Verbose "ProhibitSendReceiveQuota:  $($ProhibitSendReceive/1MB)MB"
+
+    $desc = "Raise Quota for `"$Mailbox`""
+    $caption = $desc
+    $warning = "Are you sure you want to perform this action?`n"
+    $warning += "This will raise quota limits for the user "
+    $warning += "to $($IssueWarning/1MB)MB/$($ProhibitSend/1MB)MB/$($ProhibitSendReceive/1MB)MB.`n"
+    $warning += "Current mailbox size:  $($TotalItemSize)MB"
+
+    if (!$PSCmdlet.ShouldProcess($desc, $warning, $caption)) {
+        return
+    }
+
+    Set-Mailbox -Identity $Mailbox -DomainController $dc `
+                -IssueWarningQuota $IssueWarning `
+                -ProhibitSendQuota $ProhibitSend `
+                -ProhibitSendReceiveQuota $ProhibitSendReceive `
+                -UseDatabaseQuotaDefaults:$false
+    Write-Verbose "Raised quota limits for `"$($Mailbox.Name)`"."
 }
 END {
     if ($TranscriptPath -ne $null) {
